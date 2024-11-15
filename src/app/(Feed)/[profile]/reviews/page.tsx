cadca36be4fbbaac09747abcc66ac6226ad65b71
saraf020 <sef89288@uga.edu>
2024-11-15 14:01:51 -0500

    Almost done with reviews, needs testing to remove some errors

--- src/app/(Feed)/[profile]/reviews/page.tsx
+++ src/app/(Feed)/[profile]/reviews/page.tsx
@@ -1,48 +1,56 @@
-import {useEffect, useState} from 'react';
 import {getUserReviews} from "@/server/actions/reviews";
-import {getUser} from "@/app/(Feed)/[profile]/page"
 import ReviewCard, {ReviewDoc} from '@/components/ReviewCard'
 import {cookies} from "next/headers";
 import User from "@/server/models/User";
-import {redirect} from "next/navigation";
+import {redirect} from "next/navigation"
 
 interface Props {
-    params: Promise<{ profile: string }>;
+   params: Promise<{ profile: string }>;
 }
 
 
-const ReviewsPage = ()  => {
-    const [reviews, setReviews] = useState<ReviewDoc[]>([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
-    useEffect(async () => {
-            setLoading(true);
-            setError(null);
-            try {
-                const user = await cookies()
-                    .then(User.authorize)
-                    .catch(() => redirect("/login"));
-                const userReviews = await getUserReviews(user.id);
-                displayReviews(user,userReviews);
-            } catch (e) {
-                console.log(e);
-                throw new Error("An unexpected error has occurred.");
+export default function ReviewsPage () {
+    //const [reviews, setReviews] = useState<ReviewDoc[]>([]);
+    //const [loading, setLoading] = useState(true);
+    //const [error, setError] = useState(null);
+    //useEffect(() => {
+            const getReviews = async () => {
+                //setLoading(true);
+                //setError(null);
+                try {
+                    const user = await cookies()
+                        .then(User.authorize)
+                        .catch(() => redirect("/login"));
+                    const userReviews = await getUserReviews(user.id);
+                    const docReviews = userReviews as unknown as ReviewDoc[];
+                    displayReviews(docReviews);
+                    //displayReviews(user, userReviews);
+                } catch (e) {
+                    console.log(e);
+                    throw new Error("An unexpected error has occurred.");
+                    //redirect("/login"); //change to another place later, other strings are giving me errors
+                };
             }
+            getReviews();
         }
-    );
-}
+    //);
+//}
 
-function displayReviews(user: User, userReviews) {
+function displayReviews(userReviews:ReviewDoc[]) {
     return (
         <div>
-            <h1>{userReviews.length > 0 ? `{user.avatar.username}'s Reviews:` : "No Reviews Found. Please try again later."}</h1>
+            <h1>{UserReviews.length > 0
+                ? `${userReviews[0].ownerAvatar.username}'s reviews:`
+                : "No Reviews Found. Please try again later."}
+            </h1>
             {userReviews.length > 0 ? (
-                userReviews.map(review => {
-                        <ReviewCard review={review} entity={"album"}/>
-                    }
-                )) : (
-                <p>No Review's Found</p>
+                userReviews.map((review: ReviewDoc) => (
+                    <ReviewCard review={review} entity={"album"} />
+                ))
+            ) : (
+                <p>No Reviews Found</p>
             )}
         </div>
     );
 }
+
